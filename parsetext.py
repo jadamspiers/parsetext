51b695cd03a80dacef6abd21b5c05a51086f554d
jadamspiers <jadamspiers@gmail.com>
2016-07-31 09:12:16 -0600

    Separates source and target lines

--- parsetext.py
+++ parsetext.py
@@ -1,30 +1,39 @@
-def printOnce(string):
-    lines_seen = set()
-    output = open('output.txt','w')
-    for line in open('logs.txt','r'):
-        line = line.split(']').pop()
-        if line not in lines_seen:
-            outfile.write(line)
-            lines_seen.add(line)
-    outfile.close()
+# open files
+logs = open('logs.txt','r')
 
 
-
-data = ""
-count = 0
-
+data=""
 with open('logs.txt') as f:
-	for line in f:
-		data = data + line
-
-fo = open('output.txt','wb')
-
-for item in data.split('\n'):
-    if 'destIp' in item:
-        fo.write(item+'\n')
-    if 'srcVol = MAIN-NexGen-Infrastructure-' in item:
-        fo.write(item+'\n')
+    for line in f:
+        data = data + line
 
 
 
+result = open('output.txt','w')
+
+
+# strip date
+def getDate(line):
+    return line[1:30]
+
+# find line containing text
+def sepSource():
+    temp=""
+    for item in data.split('\n'):
+        if 'AsyncRepTask' in item:
+            temp = temp+item+'\n'
+    return temp
+
+def sepDest():
+    for item in data.split('\n'):
+        if 'AsyncRepDestTask' in item:
+            result.write(item+'\n')
+
+
+
+
+print(sepSource())
+
+logs.close()
+result.close()
 
